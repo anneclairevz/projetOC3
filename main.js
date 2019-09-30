05d73a2a0ecd62112eba66857d4f28e7314c146b
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-09-30 12:42:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 // Instancie ici toute la classe avec un "NEW" et en insérant les paramètres principaux
-
-let myMap = new MapClass('map', 49.034625, 2.067596, 14, "https://api.jcdecaux.com/vls/v1/stations?contract=Cergy-pontoise&apiKey=da2e9657d290c96fa3e15abe1931d2ed8399f55e");
+let mapVelos = new MapClass('map', 49.031825, 2.067596, 13, "https://api.jcdecaux.com/vls/v1/stations?contract=Cergy-pontoise&apiKey=da2e9657d290c96fa3e15abe1931d2ed8399f55e");
 mapVelos.recupererStations();
 
-let markers = new Marker(stations.positionLat, stations.positionLong, stations.status, stations.address, stations.avalableBikes);
-markers.createMarkers();
