16e5748793ad0be626976a6036ee8c1a6bf83619
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-10-14 13:09:15 +0200

    Create form.js

---
+++ form.js
@@ -0,0 +1,19 @@
+class Form {
+    constructor(station) {
+        this.station = station;
+        this.Init();
+
+    }
+    Init() {
+        document.getElementById("stationNom").innerHTML = this.station.name;
+        document.getElementById("stationAdresse").innerHTML = this.station.address;
+        document.getElementById("stationEtat").innerHTML = this.station.status;
+        document.getElementById("velosDispos").innerHTML = this.station.available_bikes;
+        document.getElementById("attachesVelos").innerHTML = this.station.available_bike_stands;
+        document.getElementById("boutonResa").addEventListener('click', function () {
+            document.getElementById("resaForm").style.display = "block";
+            let myCanvas = new Canvas();
+        });
+    }
+
+}
