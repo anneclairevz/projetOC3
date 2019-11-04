98e606d070b28fba55b19a840536e8fb9dbb6ad2
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-11-04 09:46:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
-
+    let Resa = new ResaClass();
     // Instancie ici toute la classe avec un "NEW" et en insérant les paramètres principaux
-    let mapVelos = new MapClass('map', 49.031825, 2.067596, 13, "https://api.jcdecaux.com/vls/v1/stations?contract=Cergy-pontoise&apiKey=da2e9657d290c96fa3e15abe1931d2ed8399f55e");
+    let mapVelos = new MapClass('map', 49.031825, 2.067596, 13, "https://api.jcdecaux.com/vls/v1/stations?contract=Cergy-pontoise&apiKey=da2e9657d290c96fa3e15abe1931d2ed8399f55e", Resa);
     mapVelos.recupererStations();
-
+    Resa.check();
 });
