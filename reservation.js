c51dc47835ad1fde61443d544377e1107f4043e8
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-11-04 09:44:31 +0100

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -1,11 +1,66 @@
-/*class Resa {
+class ResaClass {
     constructor() {
-        this.name = name;
-        
+        this.station;
+        this.clientName;
+        this.firstname;
+        this.date;
     }
+    Init() {
+        //local storage pr nom prenom station date
+        sessionStorage.setItem("station", this.station);
+        sessionStorage.setItem("nom", this.clientName);
+        sessionStorage.setItem("prenom", this.firstname);
+        sessionStorage.setItem("date", this.date);
+
+    }
+
+    check(champ) {
+
+        if (document.getElementById("nom").value == "") {
+            alert("Entrez votre nom pour pouvoir effectuer une réservation");
+            document.getElementById("nom").focus();
+            return false;
+        }
+
+        if (document.getElementById("prenom").value == "") {
+            alert("Vous devez entrer votre prénom pour effectuer une réservation");
+            document.getElementById("prenom").focus();
+            return false;
+        }
+
+        if (document.getElementById("canvas").value == "") {
+            alert("Votre signature est obligatoire pour pouvoir effectuer une réservation");
+            document.getElementById("canvas").focus();
+            return false;
+        }
+
+    }
+}
+
+/* Test fonction surligne champs vide 
+function surligne(champ, erreur) {
+    if (erreur)
+        champ.style.backgroundColor = "#fba";
+    else
+        champ.style.backgroundColor = "";
 }*/
 
-const elt = document.getElementById('validerResa');    // On récupère l'élément sur lequel on veut détecter le clic
-    elt.addEventListener('click', function() {          // On écoute l'événement click
-    elt.innerHTML = "C'est réservé !";               // On change le contenu de notre élément pour afficher "C'est cliqué !"
-});
+// Test on submit
+
+
+// Below Function Executes On Form Submit
+function ValidationResa() {
+    // Storing Field Values In Variables
+    let name = document.getElementById("nom").value;
+    let firstName = document.getElementById("prenom").value;
+    let canva = document.getElementById("canvas").value;
+
+    // Conditions
+    if (name != '' && firstName != '' && canva != '') {
+        alert("All type of validation has done on OnSubmit event.");
+        return true;
+    } else {
+        alert("The Contact No. must be at least 10 digit long!");
+        return false;
+    }
+}
