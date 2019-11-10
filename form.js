6f9f0a0f900c7ffddad2fa18032af26f61e72ca5
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-11-10 22:05:06 +0100

    Update form.js

--- form.js
+++ form.js
@@ -8,6 +8,7 @@ class Form {
     }
 
     Init() {
+        document.getElementById("validerResa").disabled = true;
         document.getElementById("stationNom").innerHTML = this.station.name;
         document.getElementById("stationAdresse").innerHTML = this.station.address;
         document.getElementById("stationEtat").innerHTML = this.station.status;
@@ -16,14 +17,33 @@ class Form {
         document.getElementById("boutonResa").addEventListener('click', function () {
             document.getElementById("resaForm").style.display = "block";
             this.myCanvas = new Canvas();
+            this.myCanvas.canvas.addEventListener("mouseup", this.ValidationResa.bind(this));
 
         }.bind(this));
         document.getElementById("effacerSignature").addEventListener('click', function () {
             this.myCanvas.clearCanvas();
         }.bind(this));
         document.getElementById('validerResa').addEventListener('click', function () { // On écoute l'événement click
-            this.Resa.Init(); //entrer les parametres get elt by id nom prenom station
+            this.Resa.Init(this.station, document.getElementById("nom").value, document.getElementById("prenom").value, 20);
+
+
         }.bind(this));
+
+    }
+    ValidationResa() {
+        // Storing Field Values In Variables
+        let name = document.getElementById("nom").value;
+        let firstName = document.getElementById("prenom").value;
+
+
+        // Conditions
+        if (name != '' && firstName != '' && this.myCanvas.filled == true) {
+            document.getElementById("validerResa").disabled = false;
+
+        } else {
+            alert("Merci de remplir le formulaire");
+
+        }
     }
 
 }
