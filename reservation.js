7f7f0344697e3fd0f2d4690b1ec21d3d881770e4
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-11-24 22:02:32 +0100

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -1,99 +1,81 @@
-
 class ResaClass {
     constructor() {
         this.station;
         this.clientName;
         this.firstName;
         this.date;
-        this.minutes;
+        this.timer;
+        this.myInterval;
 
     };
+
     Init(station, nom, prenom, minutes) {
         this.station = station;
         this.clientName = nom;
         this.firstName = prenom;
         this.date = Date.now();
-        this.minutes = minutes;
-
-        sessionStorage.setItem("stationNom", this.station);
+        this.timer = parseInt(minutes) * 60;
+        this.afficherResa();
+        this.chrono();
+        sessionStorage.setItem("stationNom", JSON.stringify(this.station)); //IL ENCODE L OBJECT AU FORMAT JSON
         localStorage.setItem("nom", this.clientName);
         localStorage.setItem("prenom", this.firstName);
         sessionStorage.setItem("date", this.date);
-
-        // console.log(station);
-        // console.log(clientName);
-        // console.log(firstName);
+        console.log(this.station.address);
+        console.log(this.firstName);
+        console.log(this.clientName);
     };
 
-    check() {
+    check(minutes) {
         if (sessionStorage.getItem("date") !== null) {
-            this.station = sessionStorage.getItem("stationNom");
-            this.date = sessionStorage.getItem("date");
-            this.clientName = localStorage.getItem("nom");
-            this.firstName = localStorage.getItem("prenom");
-            //appeler la fonction afficher resa
+            let now = Date.now();
+            this.timer = Math.floor((now - sessionStorage.getItem("date")) / 1000);
+            console.log(this.timer);
+            if (this.timer < parseInt(minutes) * 60) {
+                this.timer = parseInt(minutes) * 60 - this.timer;
+
+                this.station = JSON.parse(sessionStorage.getItem("stationNom"));
+                console.log(this.station);
+                this.date = sessionStorage.getItem("date");
+                this.clientName = localStorage.getItem("nom");
+                this.firstName = localStorage.getItem("prenom");
+                this.afficherResa();
+                this.chrono();
+
+            }
         }
     }
+
     //fonction afficher resa
+    afficherResa() {
+        let minutes = Math.floor(this.timer / 60) % 60;
+        let secondes = this.timer % 60;
+        document.getElementById("resaStationNom").innerHTML = this.station.address;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("secondes").innerHTML = secondes;
+    };
 
-    //fonction timer chrono
-};
+    // chrono
+    chrono() {
+        this.myInterval = setInterval(function () {
+            this.timer--;
+            this.afficherResa();
+        }.bind(this), 1000);
+        document.getElementById("annulerResa").addEventListener('click', function () {
+            /*console.log('clic');*/
+            this.clear()
+
+        }.bind(this));
 
 
-
-
-
-
-
-
-
-/*
-class ResaClass {
-    constructor() {
-        this.station;
-        this.clientName;
-        this.firstname;
-        this.date;
-    }
-    Init() {
-        sessionStorage.setItem("station", this.station);
-        sessionStorage.setItem("nom", this.clientName);
-        sessionStorage.setItem("prenom", this.firstname);
-        sessionStorage.setItem("date", this.date);
 
     }
 
-    check(champ) {
-
-        if (document.getElementById("nom").value == "") {
-            alert("Entrez votre nom pour pouvoir effectuer une réservation");
-            document.getElementById("nom").focus();
-            return false;
-        }
-
-        if (document.getElementById("prenom").value == "") {
-            alert("Vous devez entrer votre prénom pour effectuer une réservation");
-            document.getElementById("prenom").focus();
-            return false;
-        }
-
-        if (document.getElementById("canvas").value == "") {
-            alert("Votre signature est obligatoire pour pouvoir effectuer une réservation");
-            document.getElementById("canvas").focus();
-            return false;
-        }
-
+    clear() {
+        clearInterval(this.myInterval);
+        sessionStorage.clear();
     }
+
+
+
 }
-
-Test fonction surligne un champs vide
-
-function surligne(champ, erreur) {
-    if (erreur)
-        champ.style.backgroundColor = "#fba";
-    else
-        champ.style.backgroundColor = "";
-}*/
-
-
-
