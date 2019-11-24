2a76bfb1c591958a172957a1b93622a56577f1ae
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-11-24 22:01:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
 
@@ -6,5 +5,5 @@ document.addEventListener('DOMContentLoaded', function () {
     // Instancie ici toute la classe avec un "NEW" et en insérant les paramètres principaux
     let mapVelos = new MapClass('map', 49.031825, 2.067596, 13, "https://api.jcdecaux.com/vls/v1/stations?contract=Cergy-pontoise&apiKey=da2e9657d290c96fa3e15abe1931d2ed8399f55e", Resa);
     mapVelos.recupererStations();
-    Resa.check();
+    Resa.check(20);
 });
