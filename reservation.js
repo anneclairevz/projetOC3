529eb1a1ada72cc55aa2acd7f5cacbfb984442fe
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-11-25 11:19:24 +0100

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -1,20 +1,22 @@
 class ResaClass {
-    constructor() {
+    constructor(minutes) {
         this.station;
         this.clientName;
         this.firstName;
         this.date;
-        this.timer;
+        this.timer = parseInt(minutes) * 60;
+        this.remain;
         this.myInterval;
 
     };
 
-    Init(station, nom, prenom, minutes) {
+    Init(station, nom, prenom) {
         this.station = station;
         this.clientName = nom;
         this.firstName = prenom;
         this.date = Date.now();
-        this.timer = parseInt(minutes) * 60;
+        this.remain = this.timer;
+        this.clear();
         this.afficherResa();
         this.chrono();
         sessionStorage.setItem("stationNom", JSON.stringify(this.station)); //IL ENCODE L OBJECT AU FORMAT JSON
@@ -26,47 +28,58 @@ class ResaClass {
         console.log(this.clientName);
     };
 
-    check(minutes) {
-        if (sessionStorage.getItem("date") !== null) {
-            let now = Date.now();
-            this.timer = Math.floor((now - sessionStorage.getItem("date")) / 1000);
-            console.log(this.timer);
-            if (this.timer < parseInt(minutes) * 60) {
-                this.timer = parseInt(minutes) * 60 - this.timer;
-
+    check() {
+        if (sessionStorage.getItem("date") !== null) { //on vérifie s'il y a une resa en cours
+            let now = Date.now(); //on récupère la date actuelle
+            let secndPast = Math.floor((now - sessionStorage.getItem("date")) / 1000); //on calcule le nmbre de secondes qui s est ecoulé entre les 2
+            console.log(secndPast);
+            if (secndPast < this.timer) { //si le temps ecoulé est inferieur au temps defini
+                this.remain = this.timer - secndPast; //on calcule le temps restant et on le reattribue au timer
                 this.station = JSON.parse(sessionStorage.getItem("stationNom"));
                 console.log(this.station);
                 this.date = sessionStorage.getItem("date");
                 this.clientName = localStorage.getItem("nom");
                 this.firstName = localStorage.getItem("prenom");
                 this.afficherResa();
-                this.chrono();
+                document.getElementById("timer").style.display = "block"; // ici on affiche le bloc du timer
+                this.chrono(); //redeclenche le chrono
 
+            } else {
+                this.clear();
             }
         }
     }
 
     //fonction afficher resa
     afficherResa() {
-        let minutes = Math.floor(this.timer / 60) % 60;
-        let secondes = this.timer % 60;
+        let minutes = Math.floor(this.remain / 60);
+        let secondes = this.remain % 60; //modulo = reste de la division
         document.getElementById("resaStationNom").innerHTML = this.station.address;
         document.getElementById("minutes").innerHTML = minutes;
         document.getElementById("secondes").innerHTML = secondes;
     };
-
     // chrono
+
     chrono() {
         this.myInterval = setInterval(function () {
-            this.timer--;
-            this.afficherResa();
+            this.remain--; //decremente la variable remain
+
+
+            if (this.remain <= 0) {
+                document.getElementById("expr").style.display = "block";
+                this.clear();
+                console.log('le temps est écoulé');
+            } else {
+                this.afficherResa();
+            }
+
+
         }.bind(this), 1000);
         document.getElementById("annulerResa").addEventListener('click', function () {
             /*console.log('clic');*/
             this.clear()
 
         }.bind(this));
-
 
 
     }
