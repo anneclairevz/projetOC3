16d9dc6cbcbca9be74327384ef23bff0e1f67a7f
Anne Claire <39369254+anneclairevz@users.noreply.github.com>
2019-12-08 14:02:25 +0100

    Update form.js

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-
+//Création de la class Form 
 class Form {
     constructor(station, Resa) {
         this.station = station;
@@ -9,9 +9,9 @@ class Form {
         this.Init();
 
     }
-
+//Les infos de la station s'affichent dans le bloc à droite de la carte
     Init() {
-        document.getElementById("validerResa").disabled = true;
+        document.getElementById("validerResa").disabled = true; //Le bouton validerResa est inactif 
         document.getElementById("stationNom").innerHTML = this.station.name;
         document.getElementById("stationAdresse").innerHTML = this.station.address;
         document.getElementById("stationEtat").innerHTML = this.station.status;
@@ -22,14 +22,15 @@ class Form {
             document.getElementById("prenom").value = this.firstName;
 
         }
+        //Quand on clicke sur le bouton Réservation on crée un canvas et on crée un eventListener dessus 
         document.getElementById("boutonResa").addEventListener('click', function () {
             document.getElementById("resaForm").style.display = "block";
-            this.myCanvas = new Canvas();
-            this.myCanvas.clearCanvas();
+            this.myCanvas = new Canvas();//Instancie l'objet canvas
+            this.myCanvas.clearCanvas();//Appelle la méthode clear
             this.myCanvas.canvas.addEventListener("mouseup", this.ValidationResa.bind(this));
             document.getElementById('validerResa').addEventListener('click', function () { // on récupère l'event du bouton validerResa
                 document.getElementById("timer").style.display = "block"; // ici on affiche le bloc du timer
-                document.getElementById("annl").style.display = "none"; // on affiche la phrase d annulation
+                document.getElementById("annl").style.display = "none"; // on masque la phrase d annulation
 
             })
             document.getElementById("annulerResa").addEventListener('click', function () { // on récupère l'event du bouton annulerResa
@@ -42,7 +43,7 @@ class Form {
         document.getElementById("effacerSignature").addEventListener('click', function () {
             this.myCanvas.clearCanvas();
         }.bind(this));
-        // event de la validation resa
+        // Quand on clicke sur validerResa on appelle la méthode Init de l'objet Resa
         document.getElementById('validerResa').addEventListener('click', function () { // On écoute l'événement click
             this.Resa.Init(this.station, document.getElementById("nom").value, document.getElementById("prenom").value);
 
@@ -50,13 +51,14 @@ class Form {
         }.bind(this));
 
     }
+
     ValidationResa() {
-        // Storing Field Values In Variables
+        // Enregistre les champs texte dans des variables
         let name = document.getElementById("nom").value;
         let firstName = document.getElementById("prenom").value;
 
 
-        // Conditions
+        // Conditions pour vérifier si les champs sont bien remplis par le client
         if (name != '' && firstName != '' && this.myCanvas.filled == true) {
             document.getElementById("validerResa").disabled = false;
 
